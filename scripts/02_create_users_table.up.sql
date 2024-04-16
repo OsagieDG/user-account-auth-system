28f5ebf37b889e55264cfd8ccedc06c5d8144b9b
osag1e <osagie6862@gmail.com>
2024-04-16 09:00:29 +0100

    feat: added test user creation that relies on a user_repo mock...

--- scripts/02_create_users_table.up.sql
+++ scripts/02_create_users_table.up.sql
@@ -3,8 +3,8 @@ CREATE SCHEMA IF NOT EXISTS auth;
 
 CREATE TABLE IF NOT EXISTS auth.users (
     id UUID NOT NULL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL,
     encryptedpassword VARCHAR(60) NOT NULL,
     isadmin BOOLEAN NOT NULL DEFAULT false,
     UNIQUE (username, email)
